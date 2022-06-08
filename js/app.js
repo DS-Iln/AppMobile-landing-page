c00e41ed7887707ffa51e6763a59c947e38133a2
Denis Ilyin <DenisIlin2@yandex.ru>
2022-06-09 01:55:55 +0300

    Header almost done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
+    // top-of-site-link
+    document.querySelector('.go-to-top-link').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector('.header').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+
+    // Links scroll to sections
+    // const...
+
+    function scrollToelement(elem, offset = 150) {
+        window.scrollTo({
+            top: elem.offsetTop - offset,
+            behavior: 'smooth'
+        });
+    };
+
+    // Accordions
+    // ...
 });
